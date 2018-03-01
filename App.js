5855a32161b20a8e1aee7ad8734c41ac973aa294
Gabriel Timotei <gtimotei@zeropointdynamics.com>
2018-03-01 15:18:45 -0500

    Fixed linting errors

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font } from 'expo';
-import { Ionicons } from '@expo/vector-icons';
-import RootNavigation from './navigation/RootNavigation';
+import React from 'react'
+import { Platform, StatusBar, StyleSheet, View } from 'react-native'
+import { AppLoading, Asset, Font } from 'expo'
+import { Ionicons } from '@expo/vector-icons'
+import RootNavigation from './navigation/RootNavigation'
 
 export default class App extends React.Component {
   constructor (props) {
@@ -12,7 +12,7 @@ export default class App extends React.Component {
     }
   }
 
-  render() {
+  render () {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
       return (
         <AppLoading
@@ -24,7 +24,7 @@ export default class App extends React.Component {
     } else {
       return (
         <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          {Platform.OS === 'ios' && <StatusBar barStyle='default' />}
           {Platform.OS === 'android' && <View style={styles.statusBarUnderlay} />}
           <RootNavigation />
         </View>
@@ -38,36 +38,36 @@ export default class App extends React.Component {
     return Promise.all([
       Asset.loadAsync([
         require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
+        require('./assets/images/robot-prod.png')
       ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
         ...Ionicons.font,
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
         // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
+        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf')
+      })
+    ])
   };
 
   _handleLoadingError = error => {
     // In this case, you might want to report the error to your error
     // reporting service, for example Sentry
-    console.warn(error);
+    console.warn(error)
   };
 
   _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
+    this.setState({ isLoadingComplete: true })
   };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff'
   },
   statusBarUnderlay: {
     height: 24,
-    backgroundColor: 'rgba(0,0,0,0.2)',
-  },
-});
+    backgroundColor: 'rgba(0,0,0,0.2)'
+  }
+})
