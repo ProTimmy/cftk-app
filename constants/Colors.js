7cdc55c02127a45b51d0f08c8bf2fb8d1f59f6a1
Gabriel Timotei <gabtimotei@gmail.com>
2018-03-01 00:55:43 -0500

    Initial commit

---
+++ constants/Colors.js
@@ -0,0 +1,21 @@
+const tintColor = '#2f95dc'
+
+export default {
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+  sunshine: '#FAF288',
+  coral: '#F78A72',
+  steelBlue: '#0075A3',
+  turquoise: '#42C2CD',
+  seaFoam: '#7CC9AA',
+  charcoal: '#6D6E71',
+  carolinaBlue: '#96C0E6'
+}
