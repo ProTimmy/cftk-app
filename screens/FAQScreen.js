43be14b87aa444b8c3e5ac725631717c2c2ce979
Gabriel Timotei <gabtimotei@gmail.com>
2018-03-06 13:41:29 -0500

    Added screens

---
+++ screens/FAQScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {
+  Text
+} from 'react-native'
+
+export default class FAQScreen extends React.Component {
+  render () {
+    return (
+      <Text>Something</Text>
+    )
+  }
+}
