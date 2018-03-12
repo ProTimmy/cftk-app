2c171052dbd9270488736d12b305aca43a663def
Gabriel Timotei <gabtimotei@gmail.com>
2018-03-11 20:47:41 -0400

    Added Team Name

--- api/Convio.js
+++ api/Convio.js
@@ -35,9 +35,11 @@ export function login (username, password) {
   return sendRequest(loginRequest, loginParameters, loginRequestMethod).then(function (loginResponse) {
     if (loginResponse.loginResponse) {
       var consID = loginResponse.loginResponse.cons_id
+      var token = loginResponse.loginResponse.token
       return {
         status: true,
-        id: consID
+        id: consID,
+        token: token
       }
     } else if (loginResponse.errorResponse) {
       return {
@@ -57,10 +59,8 @@ export function logout () {
   return 'something'
 }
 
-export function getDancerInfo (consID) {
-  console.log(consID)
-
-  var dancerInfoRequest = 'https://secure2.convio.net/' + API_KEY + '/site/CRConsAPI'
+export function getDancerInfo (consID, token) {
+  var dancerInfoRequestURL = 'https://secure2.convio.net/' + API_KEY + '/site/CRTeamraiserAPI'
   var requestParameters = 'method=getParticipantProgress&' +
     'api_key=' + API_KEY + '&' +
     'v=1.0&' +
@@ -68,26 +68,68 @@ export function getDancerInfo (consID) {
     'cons_id=' + consID + '&' +
     'fr_id=' + EVENT_ID
 
-  return getName(consID).then(function (nameResponse) {
-    return sendRequest(dancerInfoRequest, requestParameters, 'POST').then(function (infoResponse) {
-      return {
-        name: nameResponse,
-        info: infoResponse
-      }
+  return getName(consID, token).then(function (nameResponse) {
+    return getTeamName(consID).then(function (teamNameResponse) {
+      return sendRequest(dancerInfoRequestURL, requestParameters, 'POST').then(function (infoResponse) {
+        return {
+          name: nameResponse,
+          teamName: teamNameResponse,
+          info: infoResponse
+        }
+      })
     })
   })
 }
 
-function getName (consID) {
-  var nameRequest = 'https://secure2.convio.net/' + API_KEY + '/site/CRConsAPI'
+export function getName (consID, token) {
+  var nameRequestURL = 'https://secure2.convio.net/' + API_KEY + '/site/CRConsAPI'
   var requestParameters = 'method=getUser&' +
     'api_key=' + API_KEY + '&' +
     'v=1.0&' +
     'response_format=json&' +
-    'cons_id=' + consID
+    'cons_id=' + consID + '&' +
+    'sso_auth_token=' + token
   var requestMethod = 'POST'
 
-  return sendRequest(nameRequest, requestParameters, requestMethod).then(function (response) {
+  return sendRequest(nameRequestURL, requestParameters, requestMethod).then(function (response) {
+    var name = response.getConsResponse.name.first + ' ' + response.getConsResponse.name.last
+    return name
+  })
+}
+
+function getTeamName (consID) {
+  var requestURL = 'https://secure2.convio.net/' + API_KEY + '/site/CRTeamraiserAPI'
+  var requestParameters = 'method=getRegisteredTeamraisers&' +
+    'api_key=' + API_KEY + '&' +
+    'v=1.0&' +
+    'response_format=json&' +
+    'cons_id=' + consID + '&'
+  var requestMethod = 'POST'
+
+  return sendRequest(requestURL, requestParameters, requestMethod).then(function (response) {
+    var teamraisers = response.getRegisteredTeamraisersResponse.teamraiser
+    for (var i = 0; i < teamraisers.length; i++) {
+      if (teamraisers[i].id === EVENT_ID) {
+        return teamraisers[i].teamName
+      }
+    }
+
+    return false
+  })
+}
+
+// Functions for the settings page
+export function updatePersonalPagePrivacy (update, token) {
+  var requestURL = 'https://secure2.convio.net/' + API_KEY + '/site/CRTeamraiserAPI'
+  var requestParameters = 'method=updatePersonalPagePrivacy&' +
+    'api_key=' + API_KEY + '&' +
+    'v=1.0&' +
+    'response_format=json&' +
+    'fr_id=' + EVENT_ID + '&' +
+    'sso_auth_token=' + token
+  var requestMethod = 'POST'
+
+  return sendRequest(requestURL, requestParameters, requestMethod).then(function (response) {
     return response
   })
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,20 +4,122 @@ import {
   Text,
   View
 } from 'react-native'
+import Pie from 'react-native-pie'
+
 import Colors from '../constants/Colors'
 import TitleText from '../components/TitleText'
+
+import { getDancerInfo } from '../api/Convio'
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null
   }
 
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      consID: this.props.navigation.state.params.id,
+      token: this.props.navigation.state.params.token,
+      name: '',
+      teamName: '',
+      personalRaised: 0,
+      personalGoal: 0,
+      personalPercentage: 0,
+      teamRaised: 0,
+      teamGoal: 0,
+      teamPercentage: 0,
+      daysLeft: 0
+    }
+  }
+
+  componentWillMount () {
+    let that = this
+    getDancerInfo(this.state.consID, this.state.token).then(function (response) {
+      var personal = response.info.getParticipantProgressResponse.personalProgress
+      var team = response.info.getParticipantProgressResponse.teamProgress
+      var personalPercentage = Math.trunc((personal.raised / personal.goal) * 100)
+      var teamPercentage = Math.trunc((team.raised / team.goal) * 100)
+
+      that.setState({
+        name: response.name,
+        teamName: response.teamName,
+        personalRaised: that.parseMoney(personal.raised),
+        personalGoal: that.parseMoney(personal.goal),
+        personalPercentage: personalPercentage,
+        teamRaised: that.parseMoney(team.raised),
+        teamGoal: that.parseMoney(team.goal),
+        teamPercentage: teamPercentage,
+        daysLeft: response.info.getParticipantProgressResponse.daysLeft
+      })
+    })
+  }
+
+  parseMoney (money) {
+    // Adds decimal point
+    var decimal = money.length - 2
+    var parsedMoney = money.substr(0, decimal) + '.' + money.substr(decimal)
+
+    // Adds commas
+    return parsedMoney.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,')
+  }
+
   render () {
-    const { params } = this.props.navigation.state
-    const consID = params.consID
     return (
       <View style={styles.container}>
-        <TitleText style={styles.title}>2018 UNC Dance Marathon</TitleText>
+        <TitleText style={styles.title} titleText={this.state.name} />
+        <View>
+          <Text style={styles.days}>Days Left: {this.state.daysLeft}</Text>
+          <Text style={styles.subtitle}>Personal Fundraising</Text>
+          <View style={styles.panel}>
+            <View style={styles.information}>
+              <View style={styles.infoBlock}>
+                <Text style={styles.infoTitle}>Raised:</Text>
+                <Text>${this.state.personalRaised}</Text>
+              </View>
+              <View style={styles.infoBlock}>
+                <Text style={styles.infoTitle}>Goal:</Text>
+                <Text style={styles.infoAmount}>${this.state.personalGoal}</Text>
+              </View>
+            </View>
+            <View style={styles.pieView}>
+              <Pie
+                radius={50}
+                innerRadius={40}
+                series={[this.state.personalPercentage]}
+                colors={[Colors.carolinaBlue]}
+                backgroundColor='#ccc' />
+              <View style={styles.gauge}>
+                <Text style={styles.gaugeText}>{this.state.personalPercentage}%</Text>
+              </View>
+            </View>
+          </View>
+          <Text style={styles.subtitle}>{this.state.teamName}</Text>
+          <View style={styles.panel}>
+            <View style={styles.information}>
+              <View style={styles.infoBlock}>
+                <Text style={styles.infoTitle}>Raised:</Text>
+                <Text>${this.state.teamRaised}</Text>
+              </View>
+              <View style={styles.infoBlock}>
+                <Text style={styles.infoTitle}>Goal:</Text>
+                <Text style={styles.infoAmount}>${this.state.teamGoal}</Text>
+              </View>
+            </View>
+            <View style={styles.pieView}>
+              <Pie
+                radius={50}
+                innerRadius={40}
+                series={[this.state.teamPercentage]}
+                colors={[Colors.carolinaBlue]}
+                backgroundColor='#ccc' />
+              <View style={styles.gauge}>
+                <Text style={styles.gaugeText}>{this.state.teamPercentage}%</Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
     )
   }
@@ -27,5 +129,51 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.background
+  },
+  days: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+    justifyContent: 'center',
+    paddingTop: 10
+  },
+  subtitle: {
+    fontWeight: 'bold',
+    fontSize: 22,
+    textAlign: 'center',
+    justifyContent: 'center',
+    paddingTop: 20,
+    textDecorationLine: 'underline'
+  },
+  panel: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 30
+  },
+  information: {
+    paddingRight: 100
+  },
+  infoTitle: {
+    fontWeight: 'bold',
+    fontSize: 18
+  },
+  infoBlock: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 10,
+    paddingTop: 10
+  },
+  gauge: {
+    position: 'absolute',
+    width: 100,
+    height: 100,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  gaugeText: {
+    backgroundColor: 'transparent',
+    color: '#000',
+    fontSize: 24
   }
 })
